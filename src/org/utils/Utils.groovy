529e067386b4a083d3c1fa04fb944fb642100e14
darkobuvac <darko.buvac@codaxy.com>
2023-03-30 20:59:40 +0200

    escaping

--- src/org/utils/Utils.groovy
+++ src/org/utils/Utils.groovy
@@ -5,20 +5,20 @@ class Utils implements Serializable {
   Utils() {}
 
   def buildSection(String name, String value, String icon){
-    return [type: "section", text: [type: "mrkdwn", text: ":${icon}: *${name}:* ${value}"]]
+    return [type: "\"section\"", text: [type: "\"mrkdwn\"", text: "\":${icon}: *${name}:* ${value}\""]]
   }
 
   def buildDivider(){
-    return [type: "divider"]
+    return [type: "\"divider\""]
   }
 
   def buildHeader(String msg){
     return [
-      "\"type\"": "\"header\"",
-      "\"text\"": [
-        "\"type\"": "\"plain_text\"",
-        "\"text\"": "\":construction: ${msg}\"",
-        "\"emoji\"": true
+      type: "\"header\"",
+      text: [
+        type: "\"plain_text\"",
+        text: "\":construction: ${msg}\"",
+        emoji: true
       ]
     ]
   }
