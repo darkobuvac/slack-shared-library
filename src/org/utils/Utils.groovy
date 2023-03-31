80e13a0dadd972567f286d051d1d7e2a4d9ffb48
darkobuvac <darko.buvac@codaxy.com>
2023-03-31 08:13:21 +0200

    adding escapes

--- src/org/utils/Utils.groovy
+++ src/org/utils/Utils.groovy
@@ -72,47 +72,47 @@ class Utils implements Serializable {
   }
 
   def buildSection(String name, String value, String icon = ""){
-    return [type: "\"section\"", text: [type: "\"mrkdwn\"", text: "\":${icon}: *${name}:* ${value}\""]]
+    return ["\"type\"": "\"section\"", "\"text\"": ["\"type\"": "\"mrkdwn\"", "\"text\"": "\":${icon}: *${name}:* ${value}\""]]
   }
 
   def buildSectionTitle(String name){
-    return [type: "\"section\"", text: [type: "\"mrkdwn\"", text: "\"*${name}:*\""]]
+    return ["\"type\"": "\"section\"", "\"text\"": ["\"type\"": "\"mrkdwn\"", "\"text\"": "\"*${name}:*\""]]
   }
 
   def buildDivider(){
-    return [type: "\"divider\""]
+    return ["\"type\"": "\"divider\""]
   }
 
   def buildHeader(String msg){
     return [
-      type: "\"header\"",
-      text: [
-        type: "\"plain_text\"",
-        text: "\":construction: ${msg}\"",
-        emoji: true
+      "\"type\"": "\"header\"",
+      "\"text\"": [
+        "\"type\"": "\"plain_text\"",
+        "\"text\"": "\":construction: ${msg}\"",
+        "\"emoji\"": true
       ]
     ]
   }
 
   def buildCommitSection(String name, String username, String commitId, String commitUrl, String commitMessage, String commitTime){
     return [
-      type: "\"section\"",
-      fields: [
+      "\"type\"": "\"section\"",
+      "\"fields\"": [
           [
-              type: "\"mrkdwn\"",
-              text: "\"*Commit ID:*\n<${commitUrl}|:link: ${commitId}>\""
+              "\"type\"": "\"mrkdwn\"",
+              "\"text\"": "\"*Commit ID:*\n<${commitUrl}|:link: ${commitId}>\""
           ],
           [
-              type: "\"mrkdwn\"",
-              text: "\"*Author:*\n:bust_in_silhouette: ${name} (${username})\""
+              "\"type\"": "\"mrkdwn\"",
+              "\"text\"": "\"*Author:*\n:bust_in_silhouette: ${name} (${username})\""
           ],
           [
-              type: "\"mrkdwn\"",
-              text: "\"*Message:*\n:pencil: ${commitMessage}\""
+              "\"type\"": "\"mrkdwn\"",
+              "\"text\"": "\"*Message:*\n:pencil: ${commitMessage}\""
           ],
           [
-              type: "\"mrkdwn\"",
-              text: "\"*Time:*\n:calendar: ${commitTime}\""
+              "\"type\"": "\"mrkdwn\"",
+              "\"text\"": "\"*Time:*\n:calendar: ${commitTime}\""
           ]
       ]
     ]
