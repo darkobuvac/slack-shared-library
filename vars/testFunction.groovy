dba93a4e96af0c2642ce911f4dd95fea7c873330
darkobuvac <darko.buvac@codaxy.com>
2023-04-01 22:49:14 +0200

    ...

--- vars/testFunction.groovy
+++ vars/testFunction.groovy
@@ -2,7 +2,7 @@ import org.components.SectionText
 import org.commons.Types
 
 Map call() {
-  SectionText section = new SectionText(text: 'Test', icon: 'rocket', textElementType: Types.PLAIN_TEXT, isBold: true)
+  SectionText section = new SectionText([text: 'Test', icon: 'rocket', textElementType: Types.PLAIN_TEXT, isBold: true])
 
   Map slackBlock = section.toSlackElement()
 
