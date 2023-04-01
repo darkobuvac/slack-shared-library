ad4258d1d29da5fe3f029ddd44b08a7600d3d7fc
darkobuvac <darko.buvac@codaxy.com>
2023-04-01 23:51:23 +0200

    test

--- vars/testFunction.groovy
+++ vars/testFunction.groovy
@@ -1,18 +1,9 @@
-import org.components.SectionText
-import org.components.Header
-import org.commons.Types
 
 import org.messages.StartBuildMessage
 
 Map call() {
-  SectionText section = new SectionText('*Project*: CxO Customer Management', 'rocket', Types.MARK_DOWN)
-  Header header = new Header('Commits', 'hash')
   StartBuildMessage startMessage = new StartBuildMessage(projct: 'CxO Customer Management')
 
-  Map slackBlock = section.toSlackElement()
-
-  echo "${slackBlock}"
-  echo "${header.toSlackElement()}"
   echo "${startMessage.toSlackBlock()}"
 
   return slackBlock
