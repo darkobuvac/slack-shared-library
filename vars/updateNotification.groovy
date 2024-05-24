37e776d47421332b98e294f2d7f5b530bdbc7856
darkobuvac <darko.buvac@codaxy.com>
2024-05-24 15:41:52 +0200

    set completed or failed message

--- vars/updateNotification.groovy
+++ vars/updateNotification.groovy
@@ -1,16 +1,26 @@
 import org.components.SectionText
 import org.commons.Types
 
-def call(List<Map> blocks = []) {
+def call(List<Map> blocks = [], int status = 1) {
 
   SectionText completedMessage = new SectionText(
       '*The build and deploy pipeline has successfully completed!*',
       'white_check_mark',
       Types.MARK_DOWN)
 
+  SectionText failedMessage = new SectionText(
+      '*The build and deploy pipeline has encountered an error and failed!*',
+      'alert',
+      Types.MARK_DOWN)
+
   blocks.remove(blocks.size() - 1)
 
-  blocks.add(completedMessage.toSlackElement())
+  if(status == 1){
+    blocks.add(completedMessage.toSlackElement())
+  }else{
+    blocks.add(failedMessage.toSlackElement())
+  }
+
 
   return blocks
 }
