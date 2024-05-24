c86e7e45f1864984d6ec65c8158b877099f60b74
darkobuvac <darko.buvac@codaxy.com>
2024-05-24 15:37:17 +0200

    update message

--- vars/updateNotification.groovy
+++ vars/updateNotification.groovy
@@ -4,7 +4,7 @@ import org.commons.Types
 def call(List<Map> blocks = []) {
 
   SectionText completedMessage = new SectionText(
-      '*The build and deploy pipeline has successfully completed*',
+      '*The build and deploy pipeline has successfully completed!*',
       'white_check_mark',
       Types.MARK_DOWN)
 
